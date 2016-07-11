b796076cc36b1a307befd7d9f3df54030a4d2f8e
Vladimir Agafonkin <agafonkin@gmail.com>
2016-07-11 14:31:46 +0300

    add tests

---
+++ test/test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var polylabel = require('../');
+var test = require('tape').test;
+
+var water1 = require('./fixtures/water1.json');
+var water2 = require('./fixtures/water2.json');
+
+test('finds pole of inaccessibility for water1 and precision 1', function (t) {
+    var p = polylabel(water1, 1);
+    t.same(p, [3865.85009765625, 2124.87841796875]);
+    t.end();
+});
+
+test('finds pole of inaccessibility for water1 and precision 50', function (t) {
+    var p = polylabel(water1, 50);
+    t.same(p, [3854.296875, 2123.828125]);
+    t.end();
+});
+
+test('finds pole of inaccessibility for water2 and default precision 1', function (t) {
+    var p = polylabel(water2);
+    t.same(p, [3263.5, 3263.5]);
+    t.end();
+});
